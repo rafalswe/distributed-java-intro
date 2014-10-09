0d6958f0bb99d9fa1b70157af1491409b15c058e
rafalswe <rafalswe@hotmail.com>
2014-10-09 13:30:20 +0200

    po zajeciach1

--- 1-threads-basics/students/src/exercise3/Main.java
+++ 1-threads-basics/students/src/exercise3/Main.java
@@ -1,8 +1,31 @@
 package exercise3;
 
+import java.util.concurrent.TimeUnit;
+
+import exercise2.MyThread;
+
+
 public class Main {
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) 
+    {
+    	Thread[] tab = new Thread[4];
+    	tab[0] = new Thread(new MyRunnable("Thread-1"));
+    	tab[1] = new Thread(new MyRunnable("Thread-2"));
+    	tab[2] = new Thread(new MyRunnable("Thread-3"));
+    	tab[3] = new Thread(new MyRunnable("Thread-4"));
+    
+	    for (int i=0; i<4; i++)
+		{
+			tab[i].start();
+		}
+	    try {
+			TimeUnit.SECONDS.sleep(3);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    System.out.println("FINISHED");
     }
 }
--- 1-threads-basics/students/src/exercise3/MyRunnable.java
+++ 1-threads-basics/students/src/exercise3/MyRunnable.java
@@ -1,12 +1,17 @@
 package exercise3;
 
 public class MyRunnable implements Runnable {
-	
+
+	public  MyRunnable(String parname)
+	{
+		new Thread(this).setName(parname);
+	}
 	public void run()
 	{
+		Thread x = new Thread(this);
 		for (int i=1; i<=10; i++)
 		{
-			System.out.println(i + ": " + new Thread(this).getName());
+			System.out.println(i + ": " + x.getName());
 		}
 	}
 }
